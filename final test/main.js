6dc74e19d76775e099fec9f184bfeca56a3a7d55
Konate Mohamed Tidiane <132458995+Konamti@users.noreply.github.com>
2025-05-01 18:15:36 +0000

    Delete final test directory

--- final test/main.js
+++
@@ -1,6 +0,0 @@
-const navToggler = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".nav");
-
-navToggler.addEventListener("click", () => {
-  nav.classList.toggle("nav--visible");
-});
